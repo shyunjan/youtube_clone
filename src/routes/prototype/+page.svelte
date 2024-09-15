93852a649ed85ab15254b25135eafc5871639738
Shin HyunJae <shyunjan@naver.com>
2024-09-15 10:54:47 +0900

    add prototype main page

---
+++ src/routes/prototype/+page.svelte
@@ -0,0 +1,9 @@
+<header class="flex h-1/5 max-w-full dark:bg-neutral-900">
+	<img src="/Youtube-logo-with-flat-design-edited.png" alt="logo" class="m-2 w-32" />
+	<article class="prose max-w-full flex-grow"></article>
+	<div class="m-2 w-8">
+		<hr class="mx-1 my-2 dark:text-white" />
+		<hr class="mx-1 my-2 dark:text-white" />
+		<hr class="mx-1 my-2 dark:text-white" />
+	</div>
+</header>
