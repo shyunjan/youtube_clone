c109a89f00047954a1f4170fe3af953d094850e7
Shin HyunJae <shyunjan@naver.com>
2024-09-20 16:22:13 +0900

    add env file and fix the Vite configuration with that

--- src/routes/prototype/+page.svelte
+++ src/routes/prototype/+page.svelte
@@ -12,12 +12,12 @@
   }}
 />
 
-<header class="flex max-w-full flex-wrap items-center dark:bg-neutral-900">
+<header class="flex h-auto max-w-full flex-wrap dark:bg-neutral-900" class:h-64={visibleMenu}>
   <img src="/Youtube-logo-with-flat-design-edited.png" alt="logo" class="mx-3 my-2 h-10 w-32" />
   <section
     role="button"
     tabindex="0"
-    class="mx-2 my-1 flex h-12 flex-grow flex-col items-end justify-evenly py-1 lg:hidden"
+    class="mx-2 my-1 flex h-12 flex-col items-end justify-evenly py-1 lg:hidden"
     on:click={toggleMenu}
     on:keypress={toggleMenu}
   >
@@ -26,11 +26,10 @@
     <span class="h-1 w-8 rounded dark:bg-white" />
   </section>
   <section
-    class="transition-height flex h-0 w-full flex-col duration-500 ease-in-out lg:visible lg:h-auto lg:w-auto lg:flex-grow lg:flex-row lg:items-center"
-    class:invisible={!visibleMenu}
-    class:h-48={visibleMenu}
+    class="flex flex-col transition-height duration-500 ease-in-out lg:flex-row lg:items-center"
+    class:hidden={!visibleMenu}
   >
-    <!-- display:none 대신에 visibility:hidden을 사용하고 height에 특정한 숫자가 지정되지 않으면 transition-[height]가 작동하지 않는다 -->
+    <!-- display:none 대신에 visibility:hidden을 사용하고 height에 특정한 숫자가 지정되지 않으면 transition-height가 작동하지 않는다 -->
     <article
       class="prose flex max-w-full flex-grow flex-col justify-end text-white lg:flex lg:flex-row lg:items-center lg:justify-center"
     >
@@ -40,17 +39,17 @@
       <div class="main-menu">FAQ</div>
       <div class="main-menu">Bookings</div>
     </article>
-    <section class="m-4 mt-2 text-center lg:mt-4">
-      <img
-        src="twitter-logo.png"
-        alt="twitter_logo"
-        class="inline w-5 cursor-pointer hover:bg-orange-400"
-      />
-      <img
-        src="facebook-logo.png"
-        alt="facebook-logo"
-        class="inline w-5 cursor-pointer hover:bg-orange-400"
-      />
-    </section>
+  </section>
+  <section class="m-4 text-center lg:inline" class:hidden={!visibleMenu}>
+    <img
+      src="twitter-logo.png"
+      alt="twitter_logo"
+      class="inline w-5 cursor-pointer hover:bg-orange-400"
+    />
+    <img
+      src="facebook-logo.png"
+      alt="facebook-logo"
+      class="inline w-5 cursor-pointer hover:bg-orange-400"
+    />
   </section>
 </header>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    server: {
+      port: Number(env.PORT)
+    },
+    plugins: [sveltekit()]
+  };
 });
