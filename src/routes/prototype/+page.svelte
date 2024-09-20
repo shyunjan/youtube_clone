f748b55d4aeb41c4bef5d9dc73ddb3cd61773d5b
Shin HyunJae <shyunjan@naver.com>
2024-09-20 10:57:35 +0900

    fix responsive design of the header of the prototype main page

--- src/routes/prototype/+page.svelte
+++ src/routes/prototype/+page.svelte
@@ -12,7 +12,7 @@
   }}
 />
 
-<header class="flex max-w-full flex-wrap dark:bg-neutral-900">
+<header class="flex max-w-full flex-wrap items-center dark:bg-neutral-900">
   <img src="/Youtube-logo-with-flat-design-edited.png" alt="logo" class="mx-3 my-2 h-10 w-32" />
   <section
     role="button"
@@ -25,24 +25,32 @@
     <span class="h-1 w-8 rounded dark:bg-white" />
     <span class="h-1 w-8 rounded dark:bg-white" />
   </section>
-  <section class="flex w-full justify-stretch lg:w-auto lg:flex-grow lg:justify-end">
+  <section
+    class="transition-height flex h-0 w-full flex-col duration-500 ease-in-out lg:visible lg:h-auto lg:w-auto lg:flex-grow lg:flex-row lg:items-center"
+    class:invisible={!visibleMenu}
+    class:h-48={visibleMenu}
+  >
+    <!-- display:none 대신에 visibility:hidden을 사용하고 height에 특정한 숫자가 지정되지 않으면 transition-[height]가 작동하지 않는다 -->
     <article
-      class="prose flex max-w-full flex-grow flex-col justify-end text-white transition-[height] duration-500 ease-in-out lg:visible lg:mb-0 lg:flex lg:h-auto lg:flex-row lg:items-center"
-      class:invisible={!visibleMenu}
-      class:h-44={visibleMenu}
-      class:h-0={!visibleMenu}
-      class:mb-4={visibleMenu}
+      class="prose flex max-w-full flex-grow flex-col justify-end text-white lg:flex lg:flex-row lg:items-center lg:justify-center"
     >
-      <!-- display:none 대신에 visibility:hidden을 사용하고 height에 특정한 숫자가 지정되지 않으면 transition-[height]가 작동하지 않는다 -->
       <div class="main-menu">Home</div>
       <div class="main-menu">Gallery</div>
       <div class="main-menu">Weddings</div>
       <div class="main-menu">FAQ</div>
       <div class="main-menu">Bookings</div>
-      <div class="not-prose mx-4 text-center">
-        <img src="twitter-logo.png" alt="twitter_logo" class="inline w-5 hover:bg-orange-400" />
-        <img src="facebook-logo.png" alt="facebook-logo" class="inline w-5 hover:bg-orange-400" />
-      </div>
     </article>
+    <section class="m-4 mt-2 text-center lg:mt-4">
+      <img
+        src="twitter-logo.png"
+        alt="twitter_logo"
+        class="inline w-5 cursor-pointer hover:bg-orange-400"
+      />
+      <img
+        src="facebook-logo.png"
+        alt="facebook-logo"
+        class="inline w-5 cursor-pointer hover:bg-orange-400"
+      />
+    </section>
   </section>
 </header>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,15 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
+  theme: {
+    extend: {
+      transitionProperty: {
+        height: 'height'
+      }
+    }
+  },
 
-	plugins: [require('@tailwindcss/typography')]
+  plugins: [require('@tailwindcss/typography')]
 } as Config;
